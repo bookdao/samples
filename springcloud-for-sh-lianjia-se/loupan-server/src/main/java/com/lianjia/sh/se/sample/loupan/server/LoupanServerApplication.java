b53adb016cf9e337a880389faea4d9543eb7e5ed
huisman <huisman@dooioo.com>
2015-12-24 18:34:19 +0800

    pom 文件更新

---
+++ springcloud-for-sh-lianjia-se/loupan-server/src/main/java/com/lianjia/sh/se/sample/loupan/server/LoupanServerApplication.java
@@ -0,0 +1,14 @@
+package com.lianjia.sh.se.sample.loupan.server;
+
+/**
+  * <p>
+  *  楼盘Server 
+  * </p>
+  * @author huisman
+  * @since 1.0.0  
+  * @Copyright (c) 2015, Lianjia Group All Rights Reserved.
+*/
+public class LoupanServerApplication  {
+  public static void main(String[] args) {
+  }
+}
