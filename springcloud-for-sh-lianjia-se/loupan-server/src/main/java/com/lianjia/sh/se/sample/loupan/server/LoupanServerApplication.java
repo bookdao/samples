60933dedff5d0a2fc8ca23daf9117b7fe12fc3ff
huisman <huisman@dooioo.com>
2015-12-27 21:26:13 +0800

    一步不动

--- springcloud-for-sh-lianjia-se/loupan-server/src/main/java/com/lianjia/sh/se/sample/loupan/server/LoupanServerApplication.java
+++ springcloud-for-sh-lianjia-se/loupan-server/src/main/java/com/lianjia/sh/se/sample/loupan/server/LoupanServerApplication.java
@@ -1,5 +1,10 @@
 package com.lianjia.sh.se.sample.loupan.server;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
   * <p>
   *  楼盘Server 
@@ -10,5 +15,11 @@ package com.lianjia.sh.se.sample.loupan.server;
 */
 public class LoupanServerApplication  {
   public static void main(String[] args) {
+  } 
+  
+  @FeignClient(url="")
+  public interface CityService{
+    @RequestMapping(value="/v1/city/{id}",method=RequestMethod.GET)
+    Object findById(@PathVariable(value="id")int id );
   }
 }
